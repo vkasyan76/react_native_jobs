1490145a969e804791404d01fe0c31da973b71c0
Valentyn Kasyan <vkasyan@gmx.de>
2023-05-19 18:53:59 +0200

    Final Project, incl. Search Functionality

---
+++ project_react_native_jobs/hook/useFetch.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import { RAPID_API_KEY } from "@env";
+
+const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      // "X-RapidAPI-Key": "600d3f0c46mshfb18fcc8c3be0bep1ffc8cjsne6397dee4449",
+      "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    console.log("rapidApiKey:", rapidApiKey);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
